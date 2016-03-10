826646fe4412e4bf8f225bd8bfc3b20679c57902
tugul <tugul@live.com>
2016-03-10 23:55:40 +0100

    Compound assignment operator does implicit cast

--- src/casting/CompoundAssignmentOperator.java
+++ src/casting/CompoundAssignmentOperator.java
@@ -10,11 +10,11 @@ public class CompoundAssignmentOperator {
         short _short = 10;
 
         _int = _int + _long;    // DOES NOT COMPILE
-        _int += _long;          // COMPILE
+        _int += _long;          // COMPILES
 
         _short = _short + 1;    // DOES NOT COMPILE
-        _short += _long;        // COMPILE
-        _short += _int;         // COMPILE
+        _short += _long;        // COMPILES
+        _short += _int;         // COMPILES
         _short ++;
     }
 }
