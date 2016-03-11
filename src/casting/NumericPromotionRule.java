1d709a4c0efe4f633e1a3df60b4a575765b18a70
tugul <tugul@live.com>
2016-03-11 18:32:40 +0100

    Different types are promoted to the large one

---
+++ src/casting/NumericPromotionRule.java
@@ -0,0 +1,21 @@
+package casting;
+
+/**
+ * - If two values are of different types, Java promotes them  to larger type.
+ * - If one is integral and other is floating-point, Java promotes to the floating-point type
+ * - Small data types (short, byte, char) are first promoted to int anytime they are used.
+ * (except for unary operators, i.e. ++short => short)
+ * - After all promotion(all become same type), resulting value will be as of the promoted data type
+ */
+public class NumericPromotionRule {
+    public static void main(String[] args) {
+        int _int = 10;
+        byte _byte = 15;
+        short _short = 20;
+
+        byte _byteTwo = _int +_byte;    // DOES NOT COMPILE
+        short _shortTwo = _short + _byte; // DOES NOT COMPILE
+
+        int _intTwo = _int + _byte;     // COMPILES
+    }
+}
