d4e0ff440a294be5fb150b37dadbbcb4eae01558
tugul <tugul@live.com>
2016-03-12 14:43:11 +0100

    String concatenation create new object. Concat. goes from LEFT to RIGHT

---
+++ src/strings/StringConcatenationRules.java
@@ -0,0 +1,31 @@
+package strings;
+
+/**
+ * String is immutable,
+ * - Any concatenation creates new object and returns its reference
+ * - Concatenation goes from LEFT to RIGHT
+ * - If both operands are numeric, + means numeric addition. If either one is String, + means concatenation
+ */
+public class StringConcatenationRules {
+    public static void main(String[] args) {
+        // + concantenation
+        String str = null;
+        str += true;
+        str += 5;
+        str += " it ";
+        str += null;
+        System.out.println(str); // nulltrue5 it null
+
+        // concantenation order: left to right
+        String b = 10 + 5 + "years old";    // 15years old
+        String c = true + "love";           // truelove
+        String d = false + true + "live";   // DOES NOT COMPILE, + can't be applied to booleans
+
+        // concat(String str)
+        String a = "globe";
+        a.concat(" time");      // doesn't change a, but create new object
+        System.out.println(a);  // still 'globe'
+
+        a.concat(5);            // DOES NOT COMPILE, takes only string parameter
+    }
+}
