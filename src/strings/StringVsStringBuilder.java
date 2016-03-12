da0a9dd91e765c3d2ec1af0fbfe88f49a51ddf49
tugul <tugul@live.com>
2016-03-12 21:38:00 +0100

    String class's methods' return value must be used, otherwise the return value is lost

---
+++ src/strings/StringVsStringBuilder.java
@@ -0,0 +1,30 @@
+package strings;
+
+/**
+ * String and StringBuilder's difference are highlighted
+ * - Concatenation create new object in String, but not in StringBuilder
+ * - String methods' return value must be used, otherwise new value is lost
+ */
+public class StringVsStringBuilder {
+    public static void main(String[] args) {
+        // 1.
+        // Immutable class, create 27 new objects, garbage collector works busy to clean them
+        // Mutable class, create only 1 object
+        String str = "";
+        for (char c='a'; c <= 'z'; c++)
+            str += c;
+
+        StringBuilder sb = new StringBuilder();
+        for (char c='a'; c <= 'z'; c++)
+            sb.append(c);
+
+        // 2.
+        // Immutable class method(i.e. concat) returns reference of newly concatenated
+        // while Mutable class method(i.e. append) returns reference of the same object changed
+        String a = "ab";
+        a = a.concat("cd"); // abcd
+
+        StringBuilder b = new StringBuilder("ab");
+        b.append("cd");     // abcd
+    }
+}
