07d4102f1a1424d1ff8bd6fc3a2978ab8d13a9c1
tugul <tugul@live.com>
2016-03-12 08:28:34 +0100

    Vargargs in main contains only user give parameters, not class namr or java command

--- src/varargs/StringArgsInMain.java
+++ src/varargs/StringArgsInMain.java
@@ -1,11 +1,11 @@
-package argument;
+package varargs;
 
 /**
  * args contains only user given arguments
  * not class name or any java commands
  */
 
-public class Argument {
+public class StringArgsInMain {
     public static void main(String... args) {
         System.out.println(args.length);
         for (int i = 0; i < args.length; i++) {
