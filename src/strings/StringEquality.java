c2a8389751ef675451497a9cd348d329f14864ee
tugul <tugul@live.com>
2016-03-13 22:10:46 +0100

    == checks reference, equals() check value for String

--- src/strings/StringEquality.java
+++ src/strings/StringEquality.java
@@ -1,8 +1,8 @@
 package strings;
 
 /**
- * == or equals do value check for String
- * == or equals do reference check for StringBuilder (because it doesn't override default behavour)
+ * == does reference check, equals() does value check for String. So later is safest to use
+ * == or equals() does reference check for StringBuilder (because it doesn't override default behavior)
  */
 public class StringEquality {
     public static void main(String[] args) {
@@ -23,5 +23,17 @@ public class StringEquality {
         System.out.println(sb1 == sb4); // true, both refer to same object
         System.out.println(sb1);    // abcdefxyz
         System.out.println(sb4);    // abcdefxyz
+
+        // == operator on String checks object reference
+        String world = " World";
+        String helloWorld = "Hello World";
+
+        System.out.println("Hello World" == helloWorld);            // true, both from string pool
+        System.out.println("Hello World" == "Hello" + " World");    // true, calculated at compile (taken from string pool)
+        System.out.println("Hello World" == "Hello" + world);       // false, concat. creates new object in runtime
+        System.out.println("Hello World" == "Hello World".trim());  // true, trim does no change, remain same object
+        System.out.println("Hello World" == helloWorld.trim());     // true, trim does no change, remain same object
+        System.out.println("Hello World" == " Hello World".trim()); // false, trim changes and returns new object
+        System.out.println("Hello World" == " Hello World".trim().intern());  // true, intern takes from string pool
     }
 }
