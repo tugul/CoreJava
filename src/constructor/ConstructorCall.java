3ca6ad626a33c2d689a117b5fb9978ee197dc26a
tugul <tugul@live.com>
2016-03-28 21:11:44 +0200

    First line in constructor is always implicit/explicit call to the parent's constructor

---
+++ src/constructor/ConstructorCall.java
@@ -0,0 +1,42 @@
+package constructor;
+
+/**
+ * First line in a constructor is implicit call (if not explicitly written) to parent class's constructor
+ * If parent class doesn't have default constructor, child constructor must EXPLICITLY call parent's non default constructor
+ */
+
+class Animal {  // Does not have default constructor
+    Animal(int i) { }
+    Animal(boolean b) { }
+}
+
+class Dog extends Animal {
+    Dog (){}          // DOES NOT COMPILE
+
+    Dog() {
+        super(6);     // Mandatory call to parent's constructor
+    }
+
+    Dog(int a) {
+        super(true);  // Mandatory call to parent's constructor
+    }
+}
+
+// Below 3 classes are equivalent because compiler converts them too last one
+class Pet1 {
+}
+
+class Pet2 {
+    Pet2() {}
+}
+
+class Pet3 {
+    Pet3(){
+        super();
+    }
+}
+
+public class ConstructorCall {
+    public static void main(String[] args) {
+    }
+}
