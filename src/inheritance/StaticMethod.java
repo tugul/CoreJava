3a35240a83e884224ce960e5dbee1dcf013de951
tugul <tugul@live.com>
2016-03-29 22:27:56 +0200

    Static method of interface must have body, and not inherited to its implementing class regardless of abstract or concrete

---
+++ src/inheritance/StaticMethod.java
@@ -0,0 +1,26 @@
+package inheritance;
+
+/**
+ * Static method of interface
+ * - Declared with static keyword
+ * - Must have a body
+ * - Assumed public
+ * - Hidden from/not inherited to implementing classes
+ * - Reference to the interface name must be used to use
+ */
+interface IHaveStatic1 {
+    static void sayMyName() { System.out.println("Interface One"); }
+}
+
+interface IHaveStatic2 {
+    static void sayMyName() { System.out.println("Interface Two"); }
+}
+
+public class StaticMethod implements IHaveStatic1, IHaveStatic2 {
+    public static void main(String[] args) {
+        // No conflict of two methods with same signature
+        // because they only belong to their own interfaces
+        IHaveStatic1.sayMyName();   // Interface One
+        IHaveStatic2.sayMyName();   // Interface Two
+    }
+}
