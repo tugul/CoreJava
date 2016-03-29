6fe8deec9106969b6642a55fefd60ac043b124e5
tugul <tugul@live.com>
2016-03-29 23:57:11 +0200

    Conflicting default methods from multiple interfaces must be overridden in implementing class or extending interface

---
+++ src/inheritance/ConflictingMethods.java
@@ -0,0 +1,35 @@
+package inheritance;
+
+/**
+ *
+ * In scenario where a class or interface using two interfaces which have same methods:
+ * 1. Static methods with same signature won't conflict
+ * 2. Default methods with same signature won't conflict. Resolution is to override the method
+ *
+ */
+
+interface IGo {
+    default int doIt() { return 0; }
+    static int goFor() { return 0; }
+}
+
+interface IDo {
+    default int doIt() { return 1; }
+    static int goFor() { return 0; }
+}
+
+interface IGoDo1 extends IGo, IDo {
+    // DOES NOT COMPILE, default methods conflict
+    // static methods don't conflict
+}
+
+interface IGoDo2 extends IGo, IDo {
+    // Resolution is to override conflicting default method
+    default int doIt() { return 2; }
+}
+
+public class ConflictingMethods {
+    public static void main(String[] args) {
+
+    }
+}
