649b486bad9298666f9d461f8fdc5cad5e38458d
tugul <tugul@live.com>
2016-03-30 23:42:38 +0200

    All methods of final class are automatically final

---
+++ src/inheritance/FinalClass.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+/**
+ * Final class
+ * - can't be extended, complete as it is
+ * - automatically its all methods will be assumed final
+ *
+ * Final method
+ * - can't be overridden
+ *
+ * Final field
+ * - can't be reassigned
+ *
+ * Common final classes:
+ * String, StringBuilder
+ */
+
+final class LastEmperor {
+    String name;
+    final int age = 199;
+    void setName(String name){this.name = name;}
+    static void delete() {}
+}
+public class FinalClass {
+    public static void main(String[] args) {
+        LastEmperor emperor = new LastEmperor();
+        emperor.setName("Karl");
+        System.out.println(emperor.age);
+        System.out.println(LastEmperor.age);    // DOES NOT COMPILE, final fields are not static
+    }
+}
