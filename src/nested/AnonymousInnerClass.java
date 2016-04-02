d2c5903531b87ab1107b7957eb0cd3b5727c9b3d
tugul <tugul@live.com>
2016-04-02 12:43:13 +0200

    Anonymous class mainly used for one time usage of implementation of interface or extention of abstract/concrete class

---
+++ src/nested/AnonymousInnerClass.java
@@ -0,0 +1,73 @@
+package nested;
+
+/**
+ * Anonymous inner class / Anonymous class
+ * - Doesn't have name and defined in a method
+ * - Useful when no need to create separate class for implementing interface because it will be used only once
+ * - Mainly for interface implementation to be used locally in a method
+ * - It can implement interface or extend class (abstract/concrete)
+ * - Its implementation can not add new method or field, but not accessible from outside
+ *
+ */
+
+interface A {
+    void printMyName();
+}
+
+abstract class B {
+    abstract void sayHi();
+
+    void printMyName(){
+        System.out.println("My name: abstract B");
+    }
+}
+
+class C {
+    void printMyName() {
+        System.out.println("My name: C");
+    }
+}
+
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        // Anonymous class implementing interface
+        A a = new A() {
+            public void printMyName(){
+                System.out.println("My name: A");
+            }
+            void sayHi(){
+                System.out.println("Hi A");
+            }
+        };
+        a.printMyName();
+        a.sayHi();      // DOES NOT COMPILE, interface doesn't define this method
+
+        // Anonymous class extending abstract class
+        B b = new B() {
+            int value;
+            public void printMyName(){
+                System.out.println("My name: concrete B");
+            }
+
+            public void sayHi(){
+                System.out.println("Hi B");
+            }
+        };
+        b.printMyName();
+        b.sayHi();
+        System.out.println(b.value);    // DOES NOT COMPILE, class B doesn't define this field
+
+        C c = new C(){
+            void printMyName() {
+                System.out.println("Hello C");
+            }
+
+            public void sayHi(){
+                System.out.println("Hi C");
+            }
+        };
+        c.printMyName();
+        c.sayHi();      // DOES NOT COMPILE, class C doesn't define this method
+
+    }
+}
