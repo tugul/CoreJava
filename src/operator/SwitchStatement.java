2f364069488c5461a41fbb5121974508bb6ee6f0
bayarbuyan <ubbn@live.com>
2016-04-05 15:49:48 +0200

    Switch's case label only allows constant if variable

--- src/operator/SwitchStatement.java
+++ src/operator/SwitchStatement.java
@@ -2,26 +2,44 @@ package operator;
 
 /**
  * Switch statement's decision variable:
- * int, byte, short, char, String, Enum and Integer, Byte, Short, Charactar
+ * - int, byte, short, char, String, Enum and Integer, Byte, Short, Character
+ * - long, double, float, boolean and their wrapper classes are not allowed
  *
- * long, double, float, boolean and their wrapper classes are not allowed
+ * Case label:
+ * - Allows only constant expression, such as any non-constant variables
+ * - must be unique
  */
 public class SwitchStatement {
     public static void main(String[] args) {
         int _int = 5;
+        final byte _byte = 10;
         switch (_int){
+            case 1:
+                System.out.println("One");
+            case 2:
+                System.out.println("Two");
+            case 2:                 // DOES NOT COMPILE, duplicate label
+            case _int:              // DOES NOT COMPILE, constant expression required
+            case args.length:       // DOES NOT COMPILE, constant expression required
+            case "a":               // DOES NOT COMPILE, incompatible type
+            case _byte:
+        }
+
+        long _long = 5;
+        switch (_long){     // DOES NOT COMPILE, long not allowed
             case 1:
                 System.out.println("One");
             case 2:
                 System.out.println("Other");
         }
 
-        long _long = 5;
-        switch (_long){     // DOES NOT COMPILE
-            case 1:
-                System.out.println("One");
-            case 2:
-                System.out.println("Other");
+        String a = "a";
+        String b = "a";
+        final String c = "a";
+        switch (a){
+            case "f":
+            case b: // DOES NOT COMPILE, constant expression required
+            case c:
         }
     }
 }
