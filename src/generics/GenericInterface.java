2966a2fc14f0dafe8c8c8b4ec41979f9bc3a4139
Bayarbuyan Ulziit <ubbn@live.com>
2016-04-22 12:22:31 +0200

    3 ways to implement generic interface

---
+++ src/generics/GenericInterface.java
@@ -0,0 +1,36 @@
+package generics;
+
+/**
+ * Generic interface
+ * 3 ways to implement generic interface:
+ * 1. Concrete class provides specific type
+ * 2. Concrete class continues using generic type
+ * 3. Concrete class doesn't use generics at all
+ * Last one is old way of coding and Java 8 generates compiler warning about this raw type
+ */
+
+class Box {}
+
+interface IPackable<T> {
+    void pack(T t);
+}
+
+// 1. provides specific type
+class BoxPacker implements IPackable<Box> {
+    public void pack(Box box){}
+}
+
+// 2. continues using generic type
+class Packer<T> implements IPackable<T> {
+    public void pack(T t){}
+}
+
+// 3. not using generics triggers raw type warning
+class OldPacker implements IPackable {
+    public void pack(Object o) {}
+}
+
+public class GenericInterface {
+    public static void main(String[] args) {
+    }
+}
