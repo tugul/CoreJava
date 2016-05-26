5efd14af5df2017121690f8bc6908a31dcc9f0f2
bayarbuyan <ubbn@live.com>
2016-05-26 23:38:16 +0200

    About JDBC connection

--- src/jdbc/ConnectingToDatabase.java
+++ src/jdbc/ConnectingToDatabase.java
@@ -1,17 +1,27 @@
 package jdbc;
 
+import java.sql.*;
+
 /**
  * java.sql.Driver
  *
  * - Connection URL
  * To access db, we need to build connection URL to connect to the database
- * Connection URL is composed of 3 parts: jdbc:<database_name><database_specific_detail>
+ * Composed of 3 parts separated by colon - jdbc:<database_driver_name>:<database detail,host, port etc.>
+ *
+ * - DriverManager, DataSource
+ * Those 2 helper classes are used to create connection
  *
  */
 public class ConnectingToDatabase {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String connectionUrl = "jdbc:postgresql:devalto";
+        try (Connection conn = DriverManager.getConnection(connectionUrl);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select name from table")) {
 
-
-
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        }
     }
 }
