a4dfd50083a539c4d34d5440595c223abe194a9f
bayarbuyan <ubbn@live.com>
2016-06-08 23:10:03 +0200

    Line After explicitly thrown exception doesn't compile

--- src/exception/CatchThrowsException.java
+++ src/exception/CatchThrowsException.java
@@ -12,13 +12,19 @@ public class CatchThrowsException {
         CatchThrowsException cte = new CatchThrowsException();
 
         try {
-            cte.methodOne();;
+            cte.methodOne();
         }
         catch (newException e){
-            cte.methodOne(); // DOES NOT COMPILE, unhandled checked exception
+            cte.methodOne(); // DOES NOT COMPILE, unhandled CHECKED exception
+        }
+        catch (RuntimeException e){
+            throw new RuntimeException("catch throws Runtime exception");
+            System.out.println("Unreachable");  // DOES NOT COMPILE as this line is unreachable
+                                                // because exception is thrown explicitly in previous line
         }
         catch (Exception e){
-            cte.methodTwo();
+            cte.methodTwo();            // This method throws runtime exception
+            System.out.println("DS");   // COMPILES. Unreachable line, but previous line doesn't explicitly throw
         }
         finally {
             cte.methodTwo();
