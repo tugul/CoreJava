8b1f6b3d920f7c8f98ee0a63edec13052a4c2e03
bayarbuyan <ubbn@live.com>
2016-06-14 19:56:58 +0200

    Updated note about primitive Optional classes

--- src/streams/AboutOptional.java
+++ src/streams/AboutOptional.java
@@ -1,19 +1,35 @@
 package streams;
 
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * - Optional
  * introduced in Java 8 to reduce null pointer exceptions
- * container type wrapping a single value which might be absent or present
+ * container type wrapping a single primitive or object which might be absent or present
  * more expressive than null, so supports functional programming style
  *
  * - Caller of method returning Optional
  * should use ifPresent to check value is present or not
- * use get() returns the value
- * or use orElse, orElseGet or orElseThrow to deal with absent case
+ * get() - returns the value or it throws NoSuchElementException if empty
+ * isPresent() - check if value is present or not
+ * ifPresent(Consumer<T>) - call Consumer parameter if value is present
+ *
+ * - To deal with absent case / if value in Optional is absent
+ * orElse(T) - returns given parameter value
+ * orElseGet(Supplier<T>) - returns result of Supplier
+ * orElseThrow(Supplier<Throwable>) - throws exception
+ * ofNullable(T) - creates optional with given value, but if value is null, it creates with empty
+ *
+ * - Primitive Optional classes with their method specific to their types
+ * OptionalInt - getAsInt(), orElse(int), orElseGet(IntSupplier)
+ * OptionalLong - getAsLong(), orElse(long), orElseGet(LongSupplier)
+ * OptionalDouble - getAsDouble(),orElse(double), orElseGet(DoubleSupplier)
  *
  */
 public class AboutOptional {
@@ -26,25 +42,38 @@ public class AboutOptional {
     }
 
     public static void main(String[] args) {
+        // empty() - creates instance with empty value
+        Optional optionalEmpty = Optional.empty();
+        OptionalLong optionalLong = OptionalLong.empty();
+
+        // of(T) - creates instance with given value
+        Optional optionalDate = Optional.of(LocalDate.now());
+        OptionalInt optionalInt = OptionalInt.of(5);
+        OptionalDouble optionalDouble = OptionalDouble.of(.5);
+
         System.out.println(findAverage(10, 30));    // Optional[20.0]
         System.out.println(findAverage());          // Optional.empty
 
         Double avg1 = findAverage(10, 20).get();    // 20.0
-        Double avg2 = findAverage().get();          // NoSuchElementException
+        //Double avg2 = findAverage().get();          // NoSuchElementException
 
-        Optional<Double> avgOpt = findAverage();
-        if (avgOpt.isPresent())
-            System.out.println(avgOpt.get());
-        avgOpt.ifPresent(System.out::println);   // no result
+        // isPresent() - check if has value or not
+        Optional<Double> emptyOptional = findAverage();
+        if (emptyOptional.isPresent())
+            System.out.println(emptyOptional.get());    // without check, it will throw NoSuchElementException
 
-        System.out.println(avgOpt.orElse(Double.NaN));
-        System.out.println(avgOpt.orElseGet(() -> Math.random()));  // orElseGet takes double
-        System.out.println(avgOpt.orElseThrow(() -> new IllegalStateException()));
+        // ifPresent(Consumer<T>) - call Consumer parameter if there is value present, otherwise won't call
+        emptyOptional.ifPresent(System.out::println);   // no result
 
+        System.out.println(emptyOptional.orElse(Double.NaN));
+        System.out.println(emptyOptional.orElseGet(() -> Math.random()));  // orElseGet takes double
+        System.out.println(emptyOptional.orElseThrow(() -> new IllegalStateException()));      // using lambda
+        System.out.println(emptyOptional.orElseThrow(IllegalStateException::new));             // method reference
+
+        // ofNullable(T)
         // Ternary operator to avoid null can be replaced with Optional.ofNullable(...)
         Object value = null;
         Optional obj1 = (value== null) ? Optional.empty(): Optional.of(value);
         Optional obj2 = Optional.ofNullable(value);
-
     }
 }
