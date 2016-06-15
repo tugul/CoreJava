d7c1e165528fb0bb6070c70375c98dc07530caca
bayarbuyan <ubbn@live.com>
2016-06-15 17:04:26 +0200

    Updated note about Serialization

--- src/io/Serialization.java
+++ src/io/Serialization.java
@@ -2,6 +2,7 @@ package io;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,9 +11,10 @@ import java.util.List;
  * Objects to be serialized must be type of Serializable, i.o. must implement interface Serializable
  *
  * Any instance variable in Serializable class must be also Serializable in order for composite object to be serialized
- * Attempt to serialize object which is not Serializable lead to throw NotSerializableException
- * transient instance variables ->  not serialized, set to default value of their types when de-serialized
- * Static variables -> not serialized, last set value reflects to to all objects when de-serialized
+ * Attempt to serialize object which is not Serializable will compile, but compiler will throw NotSerializableException
+ * 'transient' instance variables or primitives ->  not serialized, set to default value of their types when de-serialized
+ * non-transient primitives -> serialized, means last value is restored back
+ * Static variables -> not serialized, last set value reflects to all objects when de-serialized
  * final long serialVersionUID -> optional/special static member used by JVM during de-serialization
  *
  * ObjectInputStream    de-serialize stream into object
@@ -22,19 +24,23 @@ import java.util.List;
  * During de-serialization, constructor or any initializer of the serialized class won't be run
  * Just reference to non-serializable class won't trigger NotSerializableException unless not holding real object
  *
+ * Marking instance reference of non-serializable class 'transient' won't lead to throw NotSerializableException
+ * but data stored in the object will be lost during serialization.
+ *
  */
 
 class Wheel{
     // It is not Serializable and its reference is used in Car Serializable
-    // As long as the reference is not refering to object in memory, it won't lead Car class fail to compile
+    // As long as the reference is not referring to object in memory, it won't lead Car class fail to compile
 }
 
 class Car implements Serializable {
     private static final long serialVersionUID = 250L;
-    private static String model;    // String is Serializable
-    private transient int price;
-    private transient boolean isNew;
+    private static String model;           // String is Serializable
+    private transient int price;    // transient -> value won't be restored i.e default value will overwrite
+    private boolean isNew;          // non-transient - value will be restored
     private Wheel wheel;            // reference to non-serializable class
+    private List<Integer> gears;    // non-transient - value will be restored
 
     { price += 50; }
 
@@ -42,18 +48,20 @@ class Car implements Serializable {
         model = "Sedan";
         price = 100;
         isNew = true;
-        //wheel = new Wheel();      // will throw NotSerializableException
+        gears = Arrays.asList(3,4);
+        //wheel = new Wheel();      // will throw NotSerializableException, make it transient to avoid the exception
     }
 
     public Car(String model, int price, boolean isNew, Wheel wheel) {
-        model = model;
+        this.model = model;
         this.price = price;
         this.isNew = isNew;
+        gears = Arrays.asList(1,2,3);
         //this.wheel = wheel;       // will throw NotSerializableException
     }
 
     public String toString(){
-        return "Car=[model: " + model + ", price: "+ price+", new: " + isNew + " ] ";
+        return "Car=[model: " + model + ", price: "+ price+", new: " + isNew + ", gears: " + gears + " ] ";
     }
 }
 
@@ -90,11 +98,20 @@ public class Serialization {
         cars.add(new Car());
         cars.add(new Car("Hatchback", 130, false, new Wheel()));
 
-        writeCarsToFile(cars, "Cars.out");
-        readCarsFromFile("Cars.out").forEach(System.out::println);
-//        Output:
-//        Car=[model: Hatchback, price: 0, new: false ]
-//        Car=[model: Hatchback, price: 0, new: false ]
+        System.out.println("Before serialization");
+        cars.forEach(System.out::println);
+        writeCarsToFile(cars, "Cars.out");  // Serializing
 
+        System.out.println("After de-serialization");
+        readCarsFromFile("Cars.out").forEach(System.out::println);
     }
 }
+
+/*
+Before serialization
+Car=[model: Hatchback, price: 100, new: true, gears: [3, 4] ]
+Car=[model: Hatchback, price: 130, new: false, gears: [1, 2, 3] ]
+After de-serialization
+Car=[model: Hatchback, price: 0, new: true, gears: [3, 4] ]
+Car=[model: Hatchback, price: 0, new: false, gears: [1, 2, 3] ]
+*/
