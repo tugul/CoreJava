36f4ada6d8fcb1fe069a434108b713e7935de6b1
bayarbuyan <ubbn@live.com>
2016-06-28 23:54:10 +0200

    Corrected typos

--- src/generics/TypeErasure.java
+++ src/generics/TypeErasure.java
@@ -1,11 +1,10 @@
 package generics;
 
-
 /**
  * - Type erasure
  * Generic types are replaced/erased by compiler to type Object
  * In other words, generic type and Object type are same in compile time, 
- * So below it two methods have same signature 
+ * Below two methods have same signature, thus won't compile 
  */
 public class TypeErasure<T> {
 	void method(T t) {}			// DOES NOT COMPILE
