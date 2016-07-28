1f873b479adc0f5697afd18f02f43542cfa4480a
bayarbuyan <ubbn@live.com>
2016-07-28 22:30:44 +0200

    Callable.call returns generic type and can throw any exception

--- concurrency/CallableVsRunnable.java
+++ concurrency/CallableVsRunnable.java
@@ -1,17 +1,16 @@
 package concurrency;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
  * Callable and Runnable are functional interfaces used to define a task. However,
- * Callable has method call() throws Exception
- * Runnable has method run() throws no exception
+ * Callable has a method: V call() throws Exception
+ * Runnable has a method: void run()
  *
- * Below example shows this distinction
+ * Callable is introduced later in Java 5.0 to handle cases which Runnable can't. 
+ * Runnable remained for backward compatibility. Below example shows this distinction:
  *
- * NOTE: Thread.sleep(long) throws InterruptedException
  */
 public class CallableVsRunnable {
     public static void main(String[] args) {
@@ -21,11 +20,13 @@ public class CallableVsRunnable {
 
             // COMPILES
             // because JVM considers that it is submit(Callable) as it returns
+            // and Callable can throw any exception
             service.submit( () -> { Thread.sleep(500); return true; });
 
             // DOES NOT COMPILE
-            // because JVM considers that it is submit(Runnable) as nothing is return
-            // and Runnable.run() doesn't throw any exception, so it violates
+            // because JVM considers that it is submit(Runnable) as nothing is returned
+            // and Runnable.run() shouldn't throw any exception, but here that is violated
+            // because Thread.sleep(long) throws InterruptedException, a checked exception
             service.submit( () -> { Thread.sleep(500); });
         }
         finally {
