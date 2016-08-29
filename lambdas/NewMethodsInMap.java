a91cdae466855b7cad2b808a464aae7a3c2af4af
bayarbuyan <ubbn@live.com>
2016-08-29 12:19:36 +0200

    java.util.Map.merge runs mappingfunction if key exists with non-null value

--- lambdas/NewMethodsInMap.java
+++ lambdas/NewMethodsInMap.java
@@ -19,10 +19,10 @@ public class NewMethodsInMap {
 
         // putIfAbsent
         // add key/value if no value or null is associated with given key
-        hashMap.putIfAbsent("A", "AAA");
-        hashMap.putIfAbsent("B", "BBB");
-        hashMap.putIfAbsent("C", "CCC");
-        System.out.println(hashMap); // {A=***, B=BBB, C=CCC}
+        hashMap.putIfAbsent("A", "AAA");		// key A exist with non-null value, so don't touch
+        hashMap.putIfAbsent("B", "BBB");		// key B exist but with null value, so update value
+        hashMap.putIfAbsent("C", "CCC");		// key C does not exist, so put a pair in the map
+        System.out.println(hashMap); 			// {A=***, B=BBB, C=CCC}
 
         // V merge(K, V, BiFunction<V, V, V>)
         // - Retrieve value associated with given key and apply given function on retrieved value and given value,
@@ -33,14 +33,17 @@ public class NewMethodsInMap {
         Map<String, String> favoriteMaps = new HashMap<>();
         favoriteMaps.put("A", "123");
         favoriteMaps.put("B", "123");
+        favoriteMaps.put("C", null);
         String a = favoriteMaps.merge("A", "12345", mapper);    // returns 12345, update value to 12345
         String b = favoriteMaps.merge("B", "12", mapper);       // returns 123
-        String c = favoriteMaps.merge("C", "12", mapper);       // 12, add new pair: C, 12
-        System.out.println(favoriteMaps);                       // {A=12345, B=123, C=12}
+        String c = favoriteMaps.merge("C", "12", mapper);       // 12, C exists with null, add new pair: C, 12
+        String d = favoriteMaps.merge("D", "25", mapper);       // 12, D does not exist, add new pair: D, 25
+        System.out.println(favoriteMaps);                       // {A=12345, B=123, C=12, D=25}
 
         BiFunction<String, String, String> mapperNull = (v1, v2) -> null;
-        c = favoriteMaps.merge("C", "12", mapperNull);          // returns null
-        System.out.println(favoriteMaps);                       // {A=12345, B=123}
+        c = favoriteMaps.merge("C", "12", mapperNull);          // returns null, key C is deleted
+        c = favoriteMaps.merge("E", "19", mapperNull);          // returns null, E doesn't exist, so add it without running Func
+        System.out.println(favoriteMaps);                       // {A=12345, B=123, D=250, E=19}
 
         // V computeIfPresent(K, BiFunction<K, V, V>)
         // runs given function and update value if requested key is found, and returns null if not found, or null exist in value
