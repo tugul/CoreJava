3604c8cde8a065dc9792145ec53495def1d1cc04
bayarbuyan <ubbn@live.com>
2016-08-29 13:23:53 +0200

    Local inner class/anonymous class can access to any member of outer class

--- classes/nested/LocalInnerClass.java
+++ classes/nested/LocalInnerClass.java
@@ -4,22 +4,25 @@ import java.io.Serializable;
 import java.util.regex.Pattern;
 
 /**
- * Local inner class is defined within any block of method body.
- * - has no access modifier
- * - only exists when enclosing method is invoked
- * - can't be static or can't have static members, its members can have any access modifier
- * - can access to all members of outer class
- * - can access to only final or effectively final member of enclosing method
+ * - Local inner class 
+ * Is defined within any block of method body.
+ * Has no access modifier, its members can have any access modifier
+ * Only exists when enclosing method is invoked
+ * Can't be static or can't have static members
+ * Can access to all members of outer class
+ * Can access to only final or effectively final local member of a enclosing method
  */
 public class LocalInnerClass {
-    String postCodeRegex = "^[0-9]{3}(\\s|-)?[0-9]{2}$";
+    String postCodeRegex = "";
+    
 
     void ValidatePostCode(String postCode){
         String wordIs = " are ";
-        wordIs = " is ";
+        // reassigned, so no longer effective final and can't be accessed from local inner class
+        wordIs = " is ";		
 
         class PostCode implements Serializable {
-            Pattern pattern = Pattern.compile(postCodeRegex);
+            Pattern pattern = Pattern.compile(postCodeRegex);	// not effectively final, but not local, so can be accessed
             String postCode;
             public PostCode(String postCode) {
                 this.postCode = postCode;
@@ -38,9 +41,16 @@ public class LocalInnerClass {
 
     public static void main(String[] args) {
         LocalInnerClass innerClassObj = new LocalInnerClass();
+        innerClassObj.postCodeRegex = "^[0-9]{3}(\\s|-)?[0-9]{2}$";
+        
         innerClassObj.ValidatePostCode("344-56");   // true
         innerClassObj.ValidatePostCode("344 56");   // true
         innerClassObj.ValidatePostCode("34456");    // true
         innerClassObj.ValidatePostCode("344  56");  // false
     }
 }
+
+//344-56 is true
+//344 56 is true
+//34456 is true
+//344  56 is false
