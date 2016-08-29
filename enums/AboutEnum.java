42285a82982adc17922d04b08ce03d268dcd968a
bayarbuyan <ubbn@live.com>
2016-08-29 15:38:37 +0200

    Enum can have fields, methods and constructors. But its constructor is private

--- enums/AboutEnum.java
+++ enums/AboutEnum.java
@@ -2,20 +2,20 @@ package enums;
 
 /**
  *
- * Java Enum 
- * - implicitly inherit class java.lang.Enum
- * - In jvm, it is converted to class. Its elements are instance of the enumeration class.
- * - It is public(or default) static final. So can't be extended
- * - It can have members: fields, constructor and methods
- * - Its elements must be first, before any other members if any.
- * - If enum defines abstract method, each constant element must override it
- * - Its constructor can be only private. Making it protected or public lead compilation error
+ * - Java Enum 
+ * implicitly inherits java.lang.Enum class 
+ * JVM converts it to class. Its elements are instance of the enumeration class.
+ * It is public(or default) static final. So can't be extended
+ * It can have members: fields, constructors and methods
+ * Its elements must be declared first, before any other members if any.
+ * If enum defines abstract method, each constant element must override it
+ * Its constructor can be only private. Making it protected or public lead compilation error
  *
- * Enum methods:
- * - values(): returns array of all elements
- * - valueOf(String str) : parses String and converts to Enum element
- * - name() : returns name of element in String
- * - ordinal() : returns index of element
+ * - Enum methods:
+ * values(): returns array of all elements
+ * valueOf(String str) : parses String and converts to Enum element
+ * name() : returns name of element in String
+ * ordinal() : returns index of element
  */
 
 enum Planet {
@@ -68,5 +68,11 @@ public class AboutEnum {
         System.out.println(JobPosition.CTO.description);          // Technical Officer
         System.out.println(JobPosition.Architect.description);    // null
         System.out.println(JobPosition.values().length);          // 4
+        
+        switch(devOne){
+        	case Manager : System.out.println("manager");
+        	case 1: System.out.println("1");					// DOES NOT COMPILE, type mismatch
+        	case JobPosition.CTO : System.out.println("cto");	// DOES NOT COMPILE
+        }
     }
 }
