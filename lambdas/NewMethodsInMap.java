14357b401b79a851e05dc1882caa6eb75151e855
bayarbuyan <ubbn@live.com>
2016-08-29 10:52:32 +0200

    Note updated about Map's new methods introduced in Java8

--- lambdas/NewMethodsInMap.java
+++ lambdas/NewMethodsInMap.java
@@ -24,7 +24,7 @@ public class NewMethodsInMap {
         hashMap.putIfAbsent("C", "CCC");
         System.out.println(hashMap); // {A=***, B=BBB, C=CCC}
 
-        // merge
+        // V merge(K, V, BiFunction<V, V, V>)
         // - Retrieve value associated with given key and apply given function on retrieved value and given value,
         // and the function's returned value will replace the value associated with that key
         // - If the function returns null, given key will be removed from map
@@ -33,17 +33,17 @@ public class NewMethodsInMap {
         Map<String, String> favoriteMaps = new HashMap<>();
         favoriteMaps.put("A", "123");
         favoriteMaps.put("B", "123");
-        String a = favoriteMaps.merge("A", "12345", mapper);    // returns 12345
+        String a = favoriteMaps.merge("A", "12345", mapper);    // returns 12345, update value to 12345
         String b = favoriteMaps.merge("B", "12", mapper);       // returns 123
-        String c = favoriteMaps.merge("C", "12", mapper);       // 12
+        String c = favoriteMaps.merge("C", "12", mapper);       // 12, add new pair: C, 12
         System.out.println(favoriteMaps);                       // {A=12345, B=123, C=12}
 
         BiFunction<String, String, String> mapperNull = (v1, v2) -> null;
         c = favoriteMaps.merge("C", "12", mapperNull);          // returns null
         System.out.println(favoriteMaps);                       // {A=12345, B=123}
 
-        // computeIfPresent
-        // runs given function if requested key is found, and returns null if not found
+        // V computeIfPresent(K, BiFunction<K, V, V>)
+        // runs given function and update value if requested key is found, and returns null if not found, or null exist in value
         Map<String, Integer> counts1 = new HashMap<>();
         counts1.put("A", 5);
         BiFunction<String, Integer, Integer> mapper1 = (k, v) -> v * 10;
@@ -51,16 +51,16 @@ public class NewMethodsInMap {
         Integer intB = counts1.computeIfPresent("B", mapper1);  // null
         System.out.println(counts1);                            // {A=50}
 
-        // /computeIfAbsent
+        // V computeIfAbsent(K, Function<K, V>)
         // runs given function if requested key is not found or value is null, and returns null if found or non-null
         Map<String, Integer> counts2 = new HashMap<>();
         counts2.put("A", 20);
         counts2.put(null, 20);
         counts2.put("B", null);
         Function<String, Integer> mapper2 = (k) -> 9;
-        Integer intA2 = counts2.computeIfAbsent("A", mapper2); // 20
-        Integer intB2 = counts2.computeIfAbsent("B", mapper2); // 9
-        Integer intC2 = counts2.computeIfAbsent("C", mapper2); // 9
-        System.out.println(counts2);                           // {A=20, B=9, C=9}
+        Integer intA2 = counts2.computeIfAbsent("A", mapper2); // 20, A exists with non-null value, so don't touch
+        Integer intB2 = counts2.computeIfAbsent("B", mapper2); // 9, B exists, but with null value, so run Function
+        Integer intC2 = counts2.computeIfAbsent("C", mapper2); // 9, C doesn't exist, so run Function
+        System.out.println(counts2);                           // {null=20, A=20, B=9, C=9}
     }
 }
