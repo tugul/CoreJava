06de86517c14817ae9b1870f29fe4c466bc56635
bayarbuyan <ubbn@live.com>
2016-09-03 22:49:33 +0200

    Same static methods from two interfaces won't be conflicted when implemented by one class

--- interfaces/ConflictingMethods.java
+++ interfaces/ConflictingMethods.java
@@ -8,6 +8,24 @@ package interfaces;
  *
  */
 
+interface IMe {
+	static void me(){System.out.println("IMe");}
+}
+
+interface IYou {
+	default void me(){System.out.println("IYou");}
+}
+
+interface IYouAndMe extends IMe, IYou {}
+
+class Wondering implements IMe {
+	public static void me(){ System.out.println("I am wondering...");}
+}
+
+class Lost implements IYouAndMe {
+	public void me(){ System.out.println("I am lost");}
+}
+
 interface IGo {
     default int doIt() { return 0; }
     static int goFor() { return 0; }
@@ -30,6 +48,16 @@ interface IGoDo2 extends IGo, IDo {
 
 public class ConflictingMethods {
     public static void main(String[] args) {
-
+    	IMe aa = new Lost();
+    	Lost ab = new Lost();
+    	IMe.me();
+    	ab.me();
+    	
+    	Wondering.me();
+    	new Wondering().me();
+    	IMe x = new Wondering();
+    	((Wondering)x).me();
+    	x.me();		// DOES NOT COMPILE
+    	//aa.me();
     }
 }
