c570309f1e1d325bc7a37d8878cc5fecec6fcffa
tugul <showzoho@gmail.com>
2018-04-22 19:25:06 +0200

    substring's end-index is exclusive

--- strings/StringMethods.java
+++ strings/StringMethods.java
@@ -2,21 +2,21 @@ package strings;
 
 /**
  *
- * String substring(int start)
- * String substring(int start, int end) - startindex is inclusive, endindex is exclusive
+ * String substring(int startIndex)
+ * String substring(int startIndex, int endIndex)
  *
  */
 public class StringMethods {
     public static void main(String[] args) {
 
-        // substring
         // startIndex is always inclusive
-        // substring(int start)          -> inclusive start index till end
-        // substring(int start, int end) -> inclusive start till exclusive end
+        // 1. substring(int startIndex)                -> inclusive start index till end
+        // 2. substring(int startIndex, int endIndex)  -> inclusive start till exclusive end
         String s = "hmm";
         System.out.println(s.substring(1, 2));  // prints just 'm'
         System.out.println(s.substring(1));     // prints 'mm'
         System.out.println(s.substring(1, 3));  // prints 'mm'
+        System.out.println(s.substring(2, 1));  // Runtime exception : StringIndexOutOfBoundsException
         System.out.println(s.substring(1, 4));  // Runtime exception : StringIndexOutOfBoundsException
 
         // PS: substring on StringBuilder returns String, not StringBuilder object
