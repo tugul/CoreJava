a10ccf65bf5002037b1795147d3614d2d2ee78aa
tugul <showzoho@gmail.com>
2018-04-22 17:18:31 +0200

    Method chain works both String/StringBuilder

--- strings/StringVsStringBuilder.java
+++ strings/StringVsStringBuilder.java
@@ -7,29 +7,36 @@ package strings;
  */
 public class StringVsStringBuilder {
     public static void main(String[] args) {
-        // 1.
+        /////////////////////////////////////////////
+        // Difference 1:
+        /////////////////////////////////////////////
+
         // Immutable class, create 27 new objects, garbage collector works busy to clean them
-        // Mutable class, create only 1 object
         String str = "";
         for (char c='a'; c <= 'z'; c++)
             str += c;
 
+        // Mutable class, create only 1 object
         StringBuilder sb = new StringBuilder();
         for (char c='a'; c <= 'z'; c++)
             sb.append(c);
 
-        // 2.
+        /////////////////////////////////////////////
+        // Difference 2:
+        /////////////////////////////////////////////
+
         // Immutable class method(i.e. concat) returns reference of newly concatenated
-        // while Mutable class method(i.e. append) returns reference of the same object changed
         String a = "ab";
         a = a.concat("cd"); // abcd
 
+        // while Mutable class method(i.e. append) returns reference of the same object changed
         StringBuilder b = new StringBuilder("ab");
         b.append("cd");     // abcd
-        b.append(true);
-        b.append(5);
-        b.append((String)null);
-        b.append((StringBuilder)null);
+        b.append(true);     // abcdtrue
+        b.append(5);        // abcdtrue5
+        b.append((String)null);         // requires explicit cast
+        b.append((StringBuilder)null);  // requires explicit cast
+        // RESULT: abcdtrue5nullnull
 
         // Similarity is method chaining which can be done on both
         String st = " abc ".trim().toUpperCase().concat("D").substring(2);  // CD
